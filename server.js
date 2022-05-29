0033c3b99cf6e0e72082a6ef58c41592d27fa9d1
Salim Shaikh <mursalin.shaikh@outlook.com>
2022-05-30 00:48:39 +0530

    first

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-app.use(express.static(__dirname + '/dist/property-sell'));
+app.use(express.static(__dirname + '/dist/propertySell'));
 app.get('/*', function(req,res)
 
-{res.sendFile(path.join(__dirname+'/dist/property-sell/index.html'));});
+{res.sendFile(path.join(__dirname+'/dist/propertySell/index.html'));});
 
 app.listen(process.env.PORT || 8080);
